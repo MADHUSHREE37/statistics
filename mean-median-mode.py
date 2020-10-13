20975387723c184a88330b2c0dac73e034bf81f6
Madhushree C R <madhushreer37@gmail.com>
2020-10-13 16:42:36 +0530

    Update mean-median-mode.py

--- mean-median-mode.py
+++ mean-median-mode.py
@@ -3,5 +3,11 @@ items = int(input())
 numbers = list(map(int,input().split()))
 
 print(statistics.mean(numbers))
+#mean = sum(numbers)/items
 print(statistics.median(numbers))
+#index = items // 2
+#median = 0
+#if items % 2:
+#    median =  sorted(numbers)[index]
+#median =  sum(sorted(numbers)[index - 1:index + 1]) / 2
 print(max(sorted(numbers), key=numbers.count))
